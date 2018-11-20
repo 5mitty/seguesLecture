a797ba2c0019f0dc40d1a9dcc62788838a7fdad4
Jacob Smith <jsmith0361@jh-mba158-19.local>
2018-11-20 11:41:26 -0600

    Initial Commit

---
+++ seguesLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seguesLecture
+//
+//  Created by Jacob Smith on 11/20/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
