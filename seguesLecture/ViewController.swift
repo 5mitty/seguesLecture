06df7baaf0330a718366be4019414179b3c98d67
Jacob Smith <jsmith0361@jh-mba158-19.local>
2018-11-20 11:59:15 -0600

    created programmatic segue

--- seguesLecture/ViewController.swift
+++ seguesLecture/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
+            self.performSegue(withIdentifier: "FirstSegue", sender: nil)
+        }
     }
 
 
